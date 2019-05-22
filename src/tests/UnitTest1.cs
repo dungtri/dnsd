c5334f445245cc6de3f4fe083ad01ce2433a948b
dungtri <geekwhynot@gmail.com>
2019-05-22 07:27:39 +0200

    Begin a first test, define dns message query entity

--- src/tests/UnitTest1.cs
+++ src/tests/UnitTest1.cs
@@ -3,6 +3,18 @@ using Xunit;
 
 namespace tests
 {
+    public class DNSMessageQuery
+    {
+        public short TransactionID { get; set; }
+        public short Flags { get; set; }
+        public short Questions { get; set; }
+        public short RRsAnswer { get; set; }
+        public short RRsAuthority { get; set; }
+        public short RRsAdditional { get; set; }
+
+        // public Queries Queries { get; set; }
+    }
+
     public class UnitTest1
     {
         [Fact]
